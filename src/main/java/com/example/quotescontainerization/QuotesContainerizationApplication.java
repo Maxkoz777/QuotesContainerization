87db4e74401b705180b5304986dfa75ec3c43a2b
Maxim <maxkozhinov777@gmail.com>
2021-12-11 18:23:42 +0300

    Initial commit

---
+++ src/main/java/com/example/quotescontainerization/QuotesContainerizationApplication.java
@@ -0,0 +1,13 @@
+package com.example.quotescontainerization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuotesContainerizationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuotesContainerizationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/quotescontainerization/QuotesContainerizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quotescontainerization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuotesContainerizationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
