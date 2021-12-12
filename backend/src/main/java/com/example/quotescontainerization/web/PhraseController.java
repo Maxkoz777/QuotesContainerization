0469ef28b82c03fd2e817cdebd7d35f2eaaf9bca
Maxim <maxkozhinov777@gmail.com>
2021-12-12 14:00:48 +0300

    Refactoring of controller due to unsafe Optional get method

--- backend/src/main/java/com/example/quotescontainerization/web/PhraseController.java
+++ backend/src/main/java/com/example/quotescontainerization/web/PhraseController.java
@@ -1,12 +1,11 @@
 package com.example.quotescontainerization.web;
 
 import com.example.quotescontainerization.model.Phrase;
+import com.example.quotescontainerization.model.types.PhraseType;
 import com.example.quotescontainerization.service.PhraseService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,12 +22,22 @@ public class PhraseController {
 
     @GetMapping("/joke")
     public ResponseEntity<Phrase> getRandomJoke() {
-        return new ResponseEntity<>(phraseService.getRandomJoke().get(), HttpStatus.OK);
+        var joke = phraseService.getRandomJoke();
+        var defaultJoke = Phrase.builder()
+                .text("This is default joke. Unable to find something funnier(")
+                .type(PhraseType.JOKE)
+                .build();
+        return joke.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.ok(defaultJoke));
     }
 
     @GetMapping("/quote")
     public ResponseEntity<Phrase> getRandomQuote() {
-        return new ResponseEntity<>(phraseService.getRandomQuote().get(), HttpStatus.OK);
+        var quote = phraseService.getRandomQuote();
+        var defaultQuote = Phrase.builder()
+                .text("This is default quote. Unable to find something wiser(")
+                .type(PhraseType.QUOTE)
+                .build();
+        return quote.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.ok(defaultQuote));
     }
 
 }
