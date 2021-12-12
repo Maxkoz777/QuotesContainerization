45122bba86b03afb66089bd1de5048487e2bff6a
Maxim <maxkozhinov777@gmail.com>
2021-12-12 03:14:16 +0300

    Repository added

---
+++ backend/src/main/java/com/example/quotescontainerization/repository/PhraseRepository.java
@@ -0,0 +1,25 @@
+package com.example.quotescontainerization.repository;
+
+import com.example.quotescontainerization.model.Phrase;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
+
+public interface PhraseRepository extends JpaRepository<Phrase, Long> {
+
+    @Query(value = "SELECT * from phrase p " +
+                    "where p.type = 'JOKE' " +
+                    "order by RANDOM()" +
+                    "limit 1",
+            nativeQuery = true)
+    Optional<Phrase> getRandomJoke();
+
+    @Query(value = "SELECT * from phrase p " +
+            "where p.type = 'QUOTE' " +
+            "order by RANDOM()" +
+            "limit 1",
+            nativeQuery = true)
+    Optional<Phrase> getRandomQuote();
+
+}
